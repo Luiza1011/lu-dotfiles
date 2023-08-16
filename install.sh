0a3c13c86a7848672a8810429845a632d3694ce4
luiza1011 <LouiseLBlue11@outlook.com>
2023-08-16 17:14:51 -0300

    first commit

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+#commit
+ln -r -s ~/.local/lu-dotfiles/.config/sway/ ~/.config/
+
+ln -r -s ~/.local/lu-dotfiles/.config/swaync/ ~/.config/
+
+ln -r -s ~/.local/lu-dotfiles/.config/waybar/ ~/.config/
+
+ln -r -s ~/.local/lu-dotfiles/.config/rofi / ~/.config/
+
+ln -r -s ~/.local/lu-dotfiles/.zshrc ~/
+
+ln -r -s ~/.local/lu-dotfiles/.fonts/ ~/
+
+ln -r -s ~/.local/lu-dotfiles/.themes/ ~/
+
+ln -r -s ~/.local/lu-dotfiles/.icons/ ~/
