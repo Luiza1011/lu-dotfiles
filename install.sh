60d015c06128fc220cad922e1f7b545b2206534d
Luiza <114603943+luizansounds@users.noreply.github.com>
2024-08-16 23:45:19 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+flatpak install flathub app.devsuite.Ptyxis io.podman_desktop.PodmanDesktop it.mijorus.gearlever com.rtosta.zapzap com.pokemmo.PokeMMO net.kuribo64.melonDS org.keepassxc.KeePassXC com.vivaldi.Vivaldi io.github.kotatogram dev.vencord.Vesktop org.gnome.FontManager io.mpv.Mpv org.prismlauncher.PrismLauncher io.github.dweymouth.supersonic com.visualstudio.code com.valvesoftware.Steam com.calibre_ebook.calibre com.github.Flacon com.github.GradienceTeam.Gradience com.github.taiko2k.tauonmb com.github.tchx84.Flatseal com.spotify.Client com.tomjwatson.Emote com.transmissionbt.Transmission flathub com.usebottles.bottles  -y --reinstall --system
+
 ln -r -s ~/.local/lu-dotfiles/.config/sway/ ~/.config/
 echo 'linking sway to .config'
 
@@ -20,10 +24,6 @@ ln -r -s ~/.local/lu-dotfiles/.themes/ ~/
 echo 'linking .themes folder to user folder'
 ln -r -s ~/.local/lu-dotfiles/.icons/ ~/
 echo 'linking .icons folder to user folder'
-
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-
-flatpak install flathub io.podman_desktop.PodmanDesktop it.mijorus.gearlever com.rtosta.zapzap com.pokemmo.PokeMMO net.kuribo64.melonDS org.keepassxc.KeePassXC com.vivaldi.Vivaldi io.github.kotatogram dev.vencord.Vesktop org.gnome.FontManager io.mpv.Mpv org.prismlauncher.PrismLauncher io.github.dweymouth.supersonic com.visualstudio.code com.valvesoftware.Steam com.calibre_ebook.calibre com.github.Flacon com.github.GradienceTeam.Gradience com.github.taiko2k.tauonmb com.github.tchx84.Flatseal com.spotify.Client com.tomjwatson.Emote com.transmissionbt.Transmission flathub com.usebottles.bottles  -y --reinstall --system
 
 #containers
 toolbox create --image quay.io/toolbx-images/archlinux-toolbox:latest arch-box
